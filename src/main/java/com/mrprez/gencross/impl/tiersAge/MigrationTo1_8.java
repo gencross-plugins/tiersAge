9d41e047720fc09c003dc180d7b1a141927298e3
MrPrez <gagneret@yahoo.fr>
2014-09-30 02:43:28 +0200

    Update version

---
+++ src/main/java/com/mrprez/gencross/impl/tiersAge/MigrationTo1_8.java
@@ -0,0 +1,15 @@
+package com.mrprez.gencross.impl.tiersAge;
+
+import com.mrprez.gencross.Version;
+import com.mrprez.gencross.migration.MigrationPersonnage;
+import com.mrprez.gencross.migration.Migrator;
+
+public class MigrationTo1_8 implements Migrator {
+
+	@Override
+	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1,8));
+		return migrationPersonnage;
+	}
+
+}
